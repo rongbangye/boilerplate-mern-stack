e549ec21d25043c129593516089ffe49cc4872e9
rongbang <rongbang.ye@gmail.com>
2020-07-25 11:30:42 -0700

    added nodemon - auto restart server

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { User } = require('./model/user');
+
 mongoose.connect('mongodb+srv://rongbangye:abc1234@react-blog.lfmpe.mongodb.net/<dbname>?retryWrites=true&w=majority', 
     {useNewUrlParser: true }).then(() => console.log('DB connected'))
                              .catch(err => console.error('err'));
 
 
-
 app.get('/', (req, res) => {
-    res.send("hello world");
+    res.send("hello");
 });
 
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+app.use(cookieParser())
 
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userData) => {
+        if(err) return res.json ({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(5000);
